1508b57ccda18348e81fa0eb87d46df9e49d5c56
florence wangechi <florencewangechim@gmail.com>
2023-04-25 23:39:46 +0300

    Completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,11 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{}</div>
+      {projects.map(item  => {
+        <ProjectItem key={item.id} name={projects.name} about={projects.about}/>
+      })}
+        
     </div>
   );
 }
